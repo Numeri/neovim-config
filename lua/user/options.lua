5dd223824fd07089e935ce00d4d6eab85450c4be
Numeri <Numeri@users.noreply.github.com>
2023-09-24 20:49:45 +0200

    Update font

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -32,7 +32,7 @@ vim.opt.signcolumn = "yes"                      -- always show the sign column, 
 vim.opt.wrap = false                            -- display lines as one long line
 vim.opt.scrolloff = 0                           -- is one of my fav
 vim.opt.sidescrolloff = 8
-vim.opt.guifont = "monospace:h17"               -- the font used in graphical neovim applications
+vim.opt.guifont = "Berkeley Mono"               -- the font used in graphical neovim applications
 vim.opt.termbidi = true
 
 vim.opt.shortmess:append "c"
